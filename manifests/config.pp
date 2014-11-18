89bbeb90265563166a1f6e88eba7764fc58a45f3
Rob Rankin <github@undertow.ca>
2014-11-18 10:18:39 +0000

    config file template attempt 1

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,14 @@ class redis::config (
   $logfile        = $redis::logfile,
 ) {
 
+  file { $conf:
+    path    => $conf,
+    content => template('redis/redis.conf.erb'),
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+  }
+
   file { $conf_logrotate:
     path    => $conf_logrotate,
     content => template('redis/redis.logrotate.erb'),
