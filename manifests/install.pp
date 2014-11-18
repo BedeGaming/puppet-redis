83aedaab5beb160bfea509bd37053a464a54042a
Rob Rankin <github@undertow.ca>
2014-11-18 10:46:42 +0000

    slight cleanup

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,5 @@
 class redis::install (
   $package        = $redis::package,
-  $service        = $redis::service,
-  $conf           = $redis::conf,
-  $conf_logrotate = $redis::conf_logrotate,
-  $pidfile        = $redis::pidfile,
-  $logfile        = $redis::logfile,
   $package_ensure = $redis::package_ensure,
 ) {
 
